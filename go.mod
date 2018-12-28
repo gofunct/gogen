7de25530448a2d50a2af4bae17a7d28149c48072
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-28 15:27:03 -0700

    CHECKPOINT

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-kit/kit v0.8.0
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
-	github.com/gofunct/common v0.0.0-20181228220816-6bd4fcdb8bf2
+	github.com/gofunct/common v0.0.0-20181228222633-2f427457fe7d
 	github.com/golang/protobuf v1.2.0
 	github.com/google/uuid v1.1.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.6.3
@@ -21,13 +21,15 @@ require (
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kyokomi/emoji v2.0.0+incompatible // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/prometheus/common v0.0.0-20181218105931-67670fe90761 // indirect
 	github.com/spf13/afero v1.2.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/viper v1.3.1
 	go.uber.org/atomic v1.3.2 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
-	go.uber.org/zap v1.9.1
+	go.uber.org/zap v1.9.1 // indirect
 	golang.org/x/sys v0.0.0-20181221143128-b4a75ba826a6 // indirect
 	google.golang.org/genproto v0.0.0-20181221175505-bd9b4fb69e2f
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
--- vendor/github.com/gofunct/common/logging/logging.go
+++ vendor/github.com/gofunct/common/logging/logging.go
@@ -4,9 +4,32 @@ package logging
 import (
 	"github.com/dixonwille/wlog"
 	kitlog "github.com/go-kit/kit/log"
+	"github.com/kyokomi/emoji"
+	"github.com/mattn/go-isatty"
 	"log"
 	"os"
 )
+
+
+var (
+	noColor = os.Getenv("TERM") == "dumb" ||
+		(!isatty.IsTerminal(os.Stdout.Fd()) && !isatty.IsCygwinTerminal(os.Stdout.Fd()))
+)
+
+var ui = wlog.New(os.Stdin, os.Stdout, os.Stderr)
+
+var prefix = &wlog.PrefixUI{
+	LogPrefix:     emoji.Sprint(":speech_balloon:"),
+	OutputPrefix:   emoji.Sprint(":boom:"),
+	SuccessPrefix:  emoji.Sprint(":white_check_mark:"),
+	InfoPrefix:     emoji.Sprint(":wave:"),
+	ErrorPrefix:    emoji.Sprint(":x:"),
+	WarnPrefix:     emoji.Sprint(":warning:"),
+	RunningPrefix:  emoji.Sprint(":zap:"),
+	AskPrefix:      emoji.Sprint(":interrobang:"),
+	UI:            ui,
+}
+
 
 type Logger struct{
 	UI 		wlog.UI
@@ -16,20 +39,17 @@ type Logger struct{
 func NewLogger() *Logger {
 	logger := kitlog.NewJSONLogger(kitlog.NewSyncWriter(os.Stdout))
 	l := &Logger{
-		UI: &wlog.PrefixUI{
-			LogPrefix:     ":speech_balloon:",
-			OutputPrefix:  ":boom:",
-			SuccessPrefix: ":white_check_mark:",
-			InfoPrefix:    ":wave:",
-			ErrorPrefix:   ":x:",
-			WarnPrefix:    ":warning:",
-			RunningPrefix: ":zap:",
-			AskPrefix:     ":interrobang:",
-			UI:            wlog.New(os.Stdin, os.Stdout, os.Stderr),
-		},
+		UI: ui,
 		KitLog: logger,
 	}
 
 	log.SetOutput(kitlog.NewStdlibAdapter(l.KitLog))
 	return l
+}
+
+
+func (l *Logger) AddColor(optionColor, questionColor, responseColor, errorColor wlog.Color) {
+	if !noColor {
+		l.UI = wlog.AddColor(questionColor, errorColor, wlog.None, wlog.None, optionColor, responseColor, wlog.None, wlog.None, wlog.None, l.UI)
+	}
 }
