3a43b9d0b52cc126affd01568cfec2a486eae9fd
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-27 19:51:19 -0700

    init

--- cmd/docker.go
+++ cmd/docker.go
@@ -15,35 +15,19 @@
 package cmd
 
 import (
-	"fmt"
-
-	"github.com/fsouza/go-dockerclient"
 	"github.com/spf13/cobra"
 )
 
-func init() {}
+var (
+	dockerEndpoint string
+)
+
+func init() {
+	dockerCmd.PersistentFlags().StringVarP(&dockerEndpoint, "endpoint", "e", "unix:///var/run/docker.sock", "docker endpoint")
+}
 
 // dockerCmd represents the up command
 var dockerCmd = &cobra.Command{
 	Use:   "docker",
 	Short: "docker opts",
-	Run: func(cmd *cobra.Command, args []string) {
-		endpoint := "unix:///var/run/docker.sock"
-		client, err := docker.NewClient(endpoint)
-		if err != nil {
-			panic(err)
-		}
-		imgs, err := client.ListImages(docker.ListImagesOptions{All: false})
-		if err != nil {
-			panic(err)
-		}
-		for _, img := range imgs {
-			fmt.Println("ID: ", img.ID)
-			fmt.Println("RepoTags: ", img.RepoTags)
-			fmt.Println("Created: ", img.Created)
-			fmt.Println("Size: ", img.Size)
-			fmt.Println("VirtualSize: ", img.VirtualSize)
-			fmt.Println("ParentId: ", img.ParentID)
-		}
-	},
-}
+	}
